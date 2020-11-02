cee322cdd4b4591740cbe11b046ee22a16da0ee6
xiaobaowei <xiaobaowei@tencent.com>
2020-11-02 11:34:32 +0800

    [*]优化webpack配置，拆分为dev和prod两份，采用webpack-dev-server作为开发服务器

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'antd';
+import { Button, Input, Form, message } from 'antd';
 import { connect } from 'react-redux';
 import styles from './index.less?local';
 
+const FormItem = Form.Item;
+
 function Home({ history, dispatch }) {
   const [loading, setLoading] = useState(false);
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
 
   const onLogin = async () => {
     setLoading(true);
@@ -14,7 +18,8 @@ function Home({ history, dispatch }) {
       await dispatch({
         type: 'app/login',
         payload: {
-          user: { name: 'xiaobaowei' },
+          username,
+          password,
         },
       });
 
@@ -23,6 +28,7 @@ function Home({ history, dispatch }) {
       });
     } catch (e) {
       setLoading(false);
+      message.error({ content: e.message });
     }
   };
 
@@ -32,6 +38,23 @@ function Home({ history, dispatch }) {
         <div className={styles.content}>
           <h1>React Skeleton</h1>
           <div>
+            <div>
+              <FormItem>
+                <Input
+                  placeholder="请输入用户名"
+                  value={username}
+                  onChange={e => setUserName(e.target.value)}
+                />
+              </FormItem>
+              <FormItem>
+                <Input
+                  type="password"
+                  placeholder="请输入密码"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </FormItem>
+            </div>
             <Button type="primary" loading={loading} onClick={onLogin}>
               登录
             </Button>
